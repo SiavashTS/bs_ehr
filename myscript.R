e05110b653ba2f3f8b88c0ac468996ff57043176
Siavash <siavash.teimouri@gmail.com>
2018-08-30 15:06:15 +0430

    s

--- myscript.R
+++ myscript.R
@@ -1,1 +1,4 @@
-patients[,"ETHNICITY"]
+library(RUnit)
+
+mypatients <- subset(subset(patients,subset = ETHNICITY=="english"),subset = GENDER=="M")["ID"]
+myarray <- observations[,c("PATIENT","DESCRIPTION","VALUE","UNITS")]
